071498f47e4346ddf2a4dc9fa2f5f7d52411977a
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2022-12-06 02:18:37 -0500

    day 6

---
+++ day-6/q2/src/main.rs
@@ -0,0 +1,31 @@
+use std::{env, fs};
+
+fn is_unique(arr: &Vec<char>) -> bool {
+    let mut copy = arr.clone();
+    copy.sort();
+    for i in 0..copy.len() - 1 {
+        if copy[i] == copy[i + 1] {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    let input: Vec<char> = fs::read_to_string(env::args().nth(1).unwrap())
+        .unwrap()
+        .chars()
+        .collect();
+
+    let mut window_start: usize = 0;
+
+    loop {
+        if is_unique(&input[window_start..window_start + 14].to_vec()) {
+            println!("{}", window_start + 14);
+            return;
+        }
+
+        window_start += 1;
+    }
+}
